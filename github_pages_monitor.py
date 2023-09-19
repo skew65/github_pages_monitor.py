678c00ba470c0f3b35b1f3d2d0b9d59b0ef636cd
skew65 <60857681+skew65@users.noreply.github.com>
2023-09-19 13:46:31 -0500

    Update github_pages_monitor.py

--- github_pages_monitor.py
+++ github_pages_monitor.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python
 
-import sys
+import argparse
 import requests
 import os
 
 # File to store the previous version of the website's content
 previous_file = "previous_content.html"
 
-# Check if the URL argument is provided
-if len(sys.argv) < 2:
-    print("Please provide the URL of the GitHub Pages website as a command-line argument.")
-    sys.exit(1)
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Monitor changes in GitHub Pages websites.")
+parser.add_argument("urls", nargs="+", help="URLs of the GitHub Pages websites to monitor")
+args = parser.parse_args()
 
-# Extract the URL from the command-line argument
-url = sys.argv[1]
+# Iterate over each URL
+for url in args.urls:
+    # Request the HTML content of the website
+    response = requests.get(url)
+    new_content = response.text
 
-# Request the HTML content of the website
-response = requests.get(url)
-new_content = response.text
+    # Load the previous content from the file
+    try:
+        with open(previous_file, "r") as file:
+            previous_content = file.read()
+    except FileNotFoundError:
+        previous_content = ""
 
-# Load the previous content from the file
-try:
-    with open(previous_file, "r") as file:
-        previous_content = file.read()
-except FileNotFoundError:
-    previous_content = ""
+    # Compare the previous and new content to find the new text
+    if new_content != previous_content:
+        new_text = new_content.replace(previous_content, "")
+        print(f"New text found in {url}:")
+        print(new_text)
 
-# Compare the previous and new content to find the new text
-if new_content != previous_content:
-    new_text = new_content.replace(previous_content, "")
-    print("New text found:")
-    print(new_text)
-
-    # Update the previous content file with the new content
-    with open(previous_file, "w") as file:
-        file.write(new_content)
-else:
-    print("No new text found.")
+        # Update the previous content file with the new content
+        with open(previous_file, "w") as file:
+            file.write(new_content)
+    else:
+        print(f"No new text found in {url}.")
 
 # Make the script executable
 os.chmod(__file__, 0o755)
